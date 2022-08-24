14178003ccac3bb076d164410887835bbeefc03e
sauerkrautz <110214222+sauerkrautz@users.noreply.github.com>
2022-08-24 22:11:26 +0700

    Add files via upload

--- sourceCode/src/components/Todo.jsx
+++ sourceCode/src/components/Todo.jsx
@@ -2,11 +2,8 @@ import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import Draggable from "react-draggable";
 
 const Todo = ({ text, id, completeStatus, date, completeTask, removeTodo }) => {
-  useEffect(() => {}, []);
-
   const handleRemove = () => {
     removeTodo(id);
   };
@@ -16,33 +13,31 @@ const Todo = ({ text, id, completeStatus, date, completeTask, removeTodo }) => {
   };
 
   return (
-    <Draggable>
-      <motion.div
-        initial={{ y: 10, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 100 }}
-        key={id}
-        className="relative bg-white w-full min-h-[10rem] p-2 text-md rounded-lg flex flex-col gap-4 justify-between items-baseline"
-        style={{ backgroundColor: completeStatus ? "green" : "white" }}
-        onDoubleClick={handleComplete}
+    <motion.div
+      initial={{ y: 10, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 100 }}
+      key={id}
+      className="relative bg-white w-full min-h-[10rem] p-2 text-md rounded-lg flex flex-col gap-4 justify-between items-baseline"
+      style={{ backgroundColor: completeStatus ? "green" : "white" }}
+      onDoubleClick={handleComplete}
+    >
+      <p
+        className="break-words text-left"
+        style={{ color: completeStatus ? "white" : "black" }}
       >
-        <p
-          className="break-words text-left"
-          style={{ color: completeStatus ? "white" : "black" }}
-        >
-          {text}
-        </p>
-        <div className="w-full flex justify-between">
-          <div className="text-gray-500 text-sm">{date}</div>
+        {text}
+      </p>
+      <div className="w-full flex justify-between">
+        <div className="text-gray-500 text-sm">{date}</div>
 
-          <button
-            className="text-red-500 tracking-wider p-1 rounded-lg text-md font-poppins font-bold"
-            onClick={handleRemove}
-          >
-            <FontAwesomeIcon icon={faTrash} />
-          </button>
-        </div>
-      </motion.div>
-    </Draggable>
+        <button
+          className="text-red-500 tracking-wider p-1 rounded-lg text-md font-poppins font-bold"
+          onClick={handleRemove}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </div>
+    </motion.div>
   );
 };
 
